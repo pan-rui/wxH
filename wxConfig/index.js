3541a0e3e7ec562c404fda1ae991874fcd924b10
79277490@qq.com <79277490@qq.com>
2017-12-13 23:33:32 +0800

    del iml

--- wxConfig/index.js
+++ wxConfig/index.js
@@ -1,6 +1,6 @@
 module.exports = {
     token: 'banking',
-    encodingAESKey: 'FmQxl4k7CVyyeMGl3XpO5xZQXaF1oNyfkFJVSpHFds4',
+    encodingAESKey: 'uncCxIsGt3Z03U5KgPz8BjsMR7xgWbIceMb5qyPUgvM',
     appId: 'wxf498cf460cef273f',
     appSecret: '799b631deb9d0df90ce0f342473c064e',
     wxVerify: require('./lib/wechatAuth')
