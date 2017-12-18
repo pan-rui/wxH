44abe9d637d991944bb3afd88bb6e7d7c40ed12d
79277490@qq.com <prCHU123>
2017-12-18 19:01:22 +0800

    api

--- routes/index.js
+++ routes/index.js
@@ -69,7 +69,7 @@ router.post('/msg', wechat(wxConfig, function (req, res, next) {
                         ]);*/
         }
     } else {
-        let strArry = message.Content.split(':');
+        let strArry = message.Content.split(/[:：]/);
         if (strArry.length > 1) {
             if (strArry[0] == '歌'){
                 tes.getResult('http://c.y.qq.com/soso/fcgi-bin/client_search_cp?ct=24&qqmusic_ver=1298&new_json=1&remoteplace=txt.yqq.center&searchid=37602803789127241&t=0&aggr=1&cr=1&catZhida=1&lossless=0&flag_qc=0&p=1&n=20&w='+encodeURIComponent(strArry[1])+'&g_tk=5381&loginUin=0&hostUin=0&format=json&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0',
@@ -80,7 +80,7 @@ router.post('/msg', wechat(wxConfig, function (req, res, next) {
                         else{
                             music = data.data.song.list[0];
                         }
-                        this.getResult('http://c.y.qq.com/v8/fcg-bin/fcg_play_single_song.fcg?songmid='+music['mid']+'&tpl=yqq_song_detail&format=jsonp&callback=getOneSongInfoCallback&g_tk=5381&jsonpCallback=getOneSongInfoCallback&loginUin=0&hostUin=0&format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0',
+                        tes.getResult('http://c.y.qq.com/v8/fcg-bin/fcg_play_single_song.fcg?songmid='+music['mid']+'&tpl=yqq_song_detail&format=jsonp&callback=getOneSongInfoCallback&g_tk=5381&jsonpCallback=getOneSongInfoCallback&loginUin=0&hostUin=0&format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0',
                             (result)=>{
                                 let ind=result.body.indexOf('{'),data=JSON.parse(result.body.substring(ind,result.body.length-1));
                                 res.reply({
