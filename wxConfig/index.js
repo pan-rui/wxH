2322b99823bba87d87102d2d46b47ea75eaaf293
79277490@qq.com <prCHU123>
2017-12-21 14:06:21 +0800

    api

--- wxConfig/index.js
+++ wxConfig/index.js
@@ -13,7 +13,7 @@ module.exports = {
         media_id: "6knVxeY2v8rr_fIC_L3t5qgHcluCI3pzy6Hgc5ndg74",
         url: "http://mmbiz.qpic.cn/mmbiz_gif/z54ftfNIn4G7ciblkbX9n9ibLelNs001eYcn5n3CsxVGiaGHjSriaKc2SttFHRflQfiafaJ2oFd29jq9cUIm5XKhasQ/0?wx_fmt=gif"
     },
-    bottomHtml:'<div align="center"><p style="padding-left:60px;padding-right:60px;color: #90371a">市场本没有波动,参与的人多了就有了波动.当别人把肉吃完了,你再去就只能挨刀了....</p>\n' +
+    bottomHtml:'<div align="center"><p style="padding-left:30px;padding-right:30px;color: #90371a">市场本没有波动,参与的人多了就有了波动.当别人把肉吃完了,你再去就只能挨刀了....</p>\n' +
     '  <div style="display:flex;justify-content:center">\n' +
     '    <p> <img src="http://mmbiz.qpic.cn/mmbiz_jpg/z54ftfNIn4G7ciblkbX9n9ibLelNs001eYC1mbMhRlEVH8j4dLUIhfgHiaqp3JAicgP4MySG7shY5QXxVdkLgMUPyA/0?wx_fmt=jpeg"/></p>\n' +
     '    <p><img src="http://mmbiz.qpic.cn/mmbiz_gif/z54ftfNIn4G7ciblkbX9n9ibLelNs001eYcn5n3CsxVGiaGHjSriaKc2SttFHRflQfiafaJ2oFd29jq9cUIm5XKhasQ/0?wx_fmt=gif"/></p>\n' +
